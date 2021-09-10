44acd52cf3b5199d22a9d17046291db3479d759b
MartiniBnu <MartiniBnu@users.noreply.github.com>
2021-09-10 01:51:25 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.ede95fd53b7719f105c1722671a89669.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da66fda5435b3ab0124d6f1dfe297584",
+    "url": "https://martinibnu.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "c72dd100e9bf40e43952",
+    "url": "https://martinibnu.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "e8d9cbd346403017afe4",
+    "url": "https://martinibnu.github.io/task-management-frontend/static/js/2.1057f2e2.chunk.js"
+  },
+  {
+    "revision": "c72dd100e9bf40e43952",
+    "url": "https://martinibnu.github.io/task-management-frontend/static/js/main.b430f86d.chunk.js"
+  },
+  {
+    "revision": "21b9bba0d1fc2121769f",
+    "url": "https://martinibnu.github.io/task-management-frontend/static/js/runtime~main.33b8ba49.js"
+  }
+]);
